b0efeeea11136197715cf3dd6cf93bf50cadef51
ittenom <carson@southcityart.com>
2026-05-12 19:24:11 -0500

    Railway deployment setup

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :boxland, BoxlandWeb.Endpoint,
   force_ssl: [
     rewrite_on: [:x_forwarded_proto],
     exclude: [
-      # paths: ["/health"],
+      paths: ["/healthz"],
       hosts: ["localhost", "127.0.0.1"]
     ]
   ]
